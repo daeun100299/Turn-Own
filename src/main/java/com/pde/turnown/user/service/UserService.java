f0499d6d6edca454368141ed838fdd9fd1378673
daeun100299 <goode439767@gmail.com>
2024-07-08 23:22:59 +0900

    엔티티 builder 추가, 회원가입 서비스 테스트

--- src/main/java/com/pde/turnown/user/service/UserService.java
+++ src/main/java/com/pde/turnown/user/service/UserService.java
@@ -1,5 +1,6 @@
 package com.pde.turnown.user.service;
 
+import com.pde.turnown.user.dto.UserDTO;
 import com.pde.turnown.user.entity.User;
 import com.pde.turnown.user.repository.UserRepository;
 import org.springframework.stereotype.Service;
@@ -8,10 +9,24 @@ import java.util.Optional;
 
 @Service
 public class UserService {
-    private UserRepository userRepository;
+    private final UserRepository userRepository;
 
     public UserService(UserRepository userRepository) {
         this.userRepository = userRepository;
+    }
+
+    public User registUser(UserDTO userDto) {
+        User newUser = User.builder()
+                .userId(userDto.getUserId())
+                .userPass(userDto.getUserPass())
+                .userName(userDto.getUserName())
+                .userEmail(userDto.getUserEmail())
+                .userState(userDto.getUserState())
+                .build();
+
+        System.out.println(newUser);
+
+        return userRepository.save(newUser);
     }
 
     public Optional<User> findUser(String id){
